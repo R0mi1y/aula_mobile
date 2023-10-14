ca914e79bf1630294c4bea952e568e7deeb34cd4
R0mi1y <edielromily123@gmail.com>
2023-10-14 17:41:38 -0300

    página registro

--- src/main/java/com/example/applicationteste/MainActivity.java
+++ src/main/java/com/example/applicationteste/MainActivity.java
@@ -1,4 +1,7 @@
 package com.example.applicationteste;
+import android.os.Bundle;
+import android.view.View; // Importe a classe View
+import android.app.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,8 +12,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
 
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
 
+        setContentView(R.layout.register);
     }
 }
